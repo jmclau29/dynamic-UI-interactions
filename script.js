2484f5bbca8bf0b677f7c05160c1f3865910c2bd
Justin <90084211+jmclau29@users.noreply.github.com>
2025-08-05 17:50:01 +0900

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let carouselManager = () => {
+    let slides = document.getElementsByClassName('slide');
+    for (let i = 0; i < slides.length; i++) {
+        
+    }
+
+    let prevButton = document.querySelector('.prev');
+    let nextButton = document.querySelector('.next');
+
+    prevButton.addEventListener('click', () => {
+
+    })
+
+    nextButton.addEventListener('click', () => {
+        
+    })
+}
+
+carouselManager();
